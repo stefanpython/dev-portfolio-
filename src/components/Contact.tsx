fa62110b8c9eca3c887c1d72a7b1575657a9155b
stefanpython <stefanhtml@yahoo.com>
2024-09-12 15:50:17 +0300

    Implement and style contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,7 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 // Define types for form data
 interface FormData {
@@ -9,6 +11,19 @@ interface FormData {
 }
 
 export default function Contact() {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  const [iconsVisible, setIconsVisible] = useState(false);
+
+  useEffect(() => {
+    if (inView) {
+      setIconsVisible(true);
+    }
+  }, [inView]);
+
   // Form data state with types
   const [formData, setFormData] = useState<FormData>({
     name: "",
@@ -55,40 +70,75 @@ export default function Contact() {
   };
 
   return (
-    <section className="contact-section">
-      <h2>Contact Me</h2>
-      <form onSubmit={handleSubmit} className="contact-form">
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Message:
-          <textarea
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <button type="submit">Send</button>
-      </form>
-    </section>
+    <motion.section
+      ref={ref}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+      transition={{ duration: 0.7, delay: 0.3 }}
+      className="mt-40 flex flex-col md:flex-row justify-center items-center md:items-start p-8 text-white max-w-[60em]"
+    >
+      <div className="md:w-2/3 flex flex-col ">
+        <div className="flex items-center space-x-2 text-accent text-lg mb-4">
+          <div className="flex-grow border-t border-gray-700 max-w-[18em]"></div>
+          <span className="text-accent text-xl">05.</span>
+          <h2 className="text-2xl font-bold">Contact</h2>
+          <div className="flex-grow border-t border-gray-700 max-w-[18em]"></div>
+        </div>
+
+        <div className="flex flex-col justify-start items-center">
+          <form onSubmit={handleSubmit} className="flex flex-col">
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Name:</span>
+              </div>
+              <input
+                type="text"
+                name="name"
+                placeholder="Type name here..."
+                className="input input-bordered max-w-xs w-full md:w-[30em]"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </label>
+
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email:</span>
+              </div>
+              <input
+                type="email"
+                name="email"
+                placeholder="Type email here.."
+                className="input input-bordered max-w-xs w-full md:w-[20em]"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </label>
+
+            <label className="form-control">
+              <div className="label">
+                <span className="label-text">Message:</span>
+              </div>
+              <textarea
+                className="textarea textarea-bordered h-24 w-full md:w-[23em]"
+                placeholder="Type message here..."
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              />
+            </label>
+
+            <div className="flex justify-end pt-2">
+              <button className="btn  btn-success w-full" type="submit">
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </motion.section>
   );
 }
